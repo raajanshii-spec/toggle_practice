777d588084f6c12ebcc316675da52fa0c29f63ca
rajanshi awasthi <rajanshi.awasthi@arrise.com>
2026-02-11 15:02:07 +0530

    post clean up

--- src/component/ThemeToggler.tsx
+++ src/component/ThemeToggler.tsx
@@ -5,32 +5,6 @@ import { useTheme } from "../contexts/ThemeProvider";
 
   const { isDark, toggleTheme } = useTheme();
 
-  // return (
-  //   <button onClick={toggleTheme}>
-  //     {isDark ? '🌞' : '🌙'} 
-  //   </button>
-  // )
-
-  
-  
-//   const [isDark, setDark] = useState(false);
-
-//   const toggleDark = useCallback(() => {
-//     setDark(!isDark);
-
-//     localStorage.setItem("theme", isDark ? "light" : "dark");
-//   }, [isDark]);
-
-//     console.log(isDark);
-
-
-
-//   useEffect(() => {
-//     const saved = localStorage.getItem("theme");
-
-//     setDark(saved === "dark");
-//   }, []);
-
   return (
     <div className={isDark ? "new-b" : "new-w"}>
       <div className="switch">
